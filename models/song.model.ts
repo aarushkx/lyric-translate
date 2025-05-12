31166dfd26b01c5451f370832b3e929f22bddb16
Aarush Kumar <2005aarushkumar@gmail.com>
2025-05-12 21:00:31 +0530

    feat: add database connection and song model

---
+++ models/song.model.ts
@@ -0,0 +1,53 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface ISong extends Document {
+    title: string;
+    artist: string;
+    image?: string;
+    originalLyrics: string;
+    translatedLyrics?: string;
+    vocabulary: {
+        word: string;
+        meaning: string;
+    }[];
+    exampleSentences: {
+        sentence: string;
+        meaning: string;
+    }[];
+}
+
+const songSchema: Schema<ISong> = new Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        artist: {
+            type: String,
+            default: "Unknown",
+        },
+        image: String,
+        originalLyrics: {
+            type: String,
+            required: true,
+        },
+        translatedLyrics: String,
+        vocabulary: [
+            {
+                word: String,
+                meaning: String,
+            },
+        ],
+        exampleSentences: [
+            {
+                sentence: String,
+                meaning: String,
+            },
+        ],
+    },
+    { timestamps: true }
+);
+
+export const Song =
+    (mongoose.models.Song as mongoose.Model<ISong>) ||
+    mongoose.model<ISong>("Song", songSchema);
